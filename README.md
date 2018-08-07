5e1a677ba5b1885311d3022608c6788c66a73442
Ayc0 <ayc0.benj@gmail.com>
2018-08-07 16:13:44 +0200

    Add border

--- README.md
+++ README.md
@@ -9,6 +9,15 @@ const randomArray = require("@libshin/random-array");
 // OR
 import randomArray from "@libshin/random-array";
 
-randomArray([1, 2, 3, 5, 6]);
-// [(5, 3, 2)]
+// randomArray(originArray)
+randomArray([1, 2, 3, 4, 5, 6]);
+// [4, 5, 3, 2, 1]
+
+// randomArray(originArray, maxLength)
+randomArray([1, 2, 3, 4, 5, 6], 2);
+// [5, 3]
+
+// randomArray(originArray, [minLength, maxLength])
+randomArray([1, 2, 3, 4, 5, 6], [3, 5]);
+// [4, 6, 2, 3]
 ```
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const randomArray = inputArray => {
+const randomArray = (inputArray, border) => {
   if (!Array.isArray(inputArray) || inputArray.length === 0) {
     return [];
   }
+  let inf = 1;
+  let sup = inputArray.length;
+  if (border !== undefined) {
+    if (Array.isArray(border) && border.length === 2) {
+      inf = Math.max(inf, Math.min(inputArray.length - 1, border[0], border[1]));
+      sup = Math.min(sup, Math.max(border[0], border[1]));
+    } else if (Array.isArray(border) && border.length === 1) {
+      sup = Math.min(inputArray.length, border[0]);
+    } else if (typeof border === "number") {
+      sup = Math.min(inputArray.length, border);
+    }
+  }
   const array = [...inputArray];
-  const length = parseInt(Math.random() * (inputArray.length - 1), 10) + 1;
+  const length = parseInt(Math.random() * (sup - inf + 1), 10) + inf;
   const ouputArray = [];
   for (let index = 0; index < length; index++) {
     const randomInt = parseInt(Math.random() * array.length, 10);
